03b563a641e9d6be18d0fb2e7af1a7c3b438e88f
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-29 10:56:22 +0530

    Adding login page

--- src/Router/MyRoute.jsx
+++ src/Router/MyRoute.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout.jsx/MainLayout";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
 
 const MyRoute = createBrowserRouter([
     {
@@ -14,6 +15,10 @@ const MyRoute = createBrowserRouter([
             {
                 path: '/about',
                 element: <div>About</div>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             }
         ]
     }
